d2dd823cb5084c157b1f550d937a67f3cf649825
Tunkert <tunkert@gmail.com>
2022-11-23 13:31:36 -0700

    first commit

---
+++ 002_variables/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// variables must be used
+	var name string = "Timothy Unkert"
+	// go is a statically typed language
+	fmt.Println(name)
+	fmt.Println("Hi, my name is " + name + "!") // string concatenation
+}
